d7f035e9279eb111c61efa12f3648f969c52d275
fuurin <komatsu0624@docomo.ne/jp>
2015-08-28 17:42:46 +0300

    ユーザ登録機能

--- js/register.js
+++ js/register.js
@@ -1,28 +1,27 @@
 //phpに送信
 $( function() {
-
-	//データ生成
-	var form_data = {
-		name: $("input[name=name]").val(),
-		password: $("input[name=password]").val(),
-		institute: $("input[name=institute]").val()
-	};
-
-	console.log(form_data);
-
 	//入力フォーマットが正しければ通信開始
 	$("#submit").on('click', function () {
+		//データ生成
+		var form_data = {
+			name: $("input[name=name]").val(),
+			password: $("input[name=password]").val(),
+			institute: $("input[name=institute]").val()
+		};
+
+		console.log(form_data);
+
 		$.ajax({
 			url: 'php/register.php',
 			dataType: 'text',
 			type: 'POST',
 			data: form_data,
 			success: function (msg) {
-				alart("success" + msg);
+				console.log("success" + msg);
 			},
 			error: function () {
-				alart("failed");
-			}
+				console.log("failed");
+			},
 			timeout: 10000,
 		})
 	})
