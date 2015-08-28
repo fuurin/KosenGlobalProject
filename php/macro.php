409eb684512fefb906929793c1841a38824cc30e
fuurin <komatsu0624@docomo.ne/jp>
2015-08-28 21:52:33 +0300

    ユーザ登録機能

--- php/macro.php
+++ php/macro.php
@@ -1,8 +1,8 @@
 <?php	
-	$dbname='phpdb';		//DB名	
-	$host='php.ci5vjjwa5osw.ap-northeast-1.rds.amazonaws.com';		//DBのIPアドレス
-	$hostname='root';		//DBユーザネーム
-	$password='E0M6g2b4r';	//DBパスワード
+	$db_name='phpdb';		//DB名	
+	$db_host='php.ci5vjjwa5osw.ap-northeast-1.rds.amazonaws.com';		//DBのIPアドレス
+	$db_hostname='root';		//DBユーザネーム
+	$db_password='E0M6g2b4r';	//DBパスワード
 	$port='3306';
 
 	//テーブル情報
--- php/register.php
+++ php/register.php
@@ -4,13 +4,13 @@
 
 	ChromePhp::log("connect");
 
-	$form_name=$_POST['name'];
-	$form_password=$_POST['password'];
-	$form_instutute=$_POST['insitute'];
+	$name=$_POST['name'];
+	$password=$_POST['password'];
+	$institute=$_POST['insitute'];
 
 	try
 	{
-		$pdo = new PDO('mysql:dbname='.$dbname.';host='.$host,$hostname,$password,
+		$pdo = new PDO('mysql:dbname='.$db_name.';host='.$db_host,$db_hostname,$db_password,
 						array(	PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode='TRADITIONAL'",
 								PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 		$pdo->query('SET NAMES utf8');
@@ -24,9 +24,9 @@
 	{
 		$stmt = $pdo->prepare("INSERT INTO ${TABLE_USERS}(name, password, institute) 
 								VALUES (:name, :password, :institute)");
-		$stmt->bindValue(':name', $form_name);
-		$stmt->bindValue(':password', $form_password);
-		$stmt->bindValue(':institute', $form_institute);
+		$stmt->bindValue(':name', $name);
+		$stmt->bindValue(':password', $password);
+		$stmt->bindValue(':institute', $institute);
 		$stmt->execute();
 
 		//データベース接続終了
@@ -37,5 +37,5 @@
 		exit($e->getMessage());
 	}
 
-	echo json_encode($form_name);
+	echo json_encode($name);
 ?>
