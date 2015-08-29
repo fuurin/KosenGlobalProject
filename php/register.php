ac778b320e16af8a1d5ebeeeabda0476c8394bf1
fuurin <komatsu0624@docomo.ne/jp>
2015-08-29 22:45:06 +0300

    try catchを1回にすればできる？

--- php/register.php
+++ php/register.php
@@ -5,21 +5,26 @@
         $password=$_POST['password'];
         $institute=$_POST['institute'];
 
-        try
-        {
+        try {
             $stmt = $pdo->prepare("INSERT INTO ${TABLE_USERS}(name, password, institute)
                                                             VALUES (:name, :password, :institute)");
             $stmt->bindValue(':name', $name);
             $stmt->bindValue(':password', $password);
             $stmt->bindValue(':institute', $institute);
             $stmt->execute();
+
+            // 検索
+            $stmt = $pdo->prepare("SELECT id　from ${TABLE_USERS} where name = :name");
+			$stmt->bindValue(':name', $name);
+			$stmt->execute();
+
+			$data = $stmt->fetch(PDO::FETCH_ASSOC);
+			$kgp_id = $data['id'];
         }
-        catch(PDOException $e) {
-                exit($e->getMessage());
-        }
+        catch(PDOException $e) { exit($e->getMessage()); }
 
         //データベース接続終了
         $pdo = null;
 
-        echo json_encode($name);
+        echo json_encode($kgp_id);
 ?>
