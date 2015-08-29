0c97c0850514ffa7c3298d294dbe74c9e979af74
fuurin <komatsu0624@docomo.ne/jp>
2015-08-29 21:48:39 +0300

    全てコピーした

--- js/register.js
+++ js/register.js
@@ -1,29 +1,26 @@
+//phpに送信
 $( function() {
-	// TODO:入力フォーマット判定
-	// #register_form input:submit
-	$("#submit").on('click', function () {
-		
-		//　データ生成
-		var form_data = {
-			name: $("input[name=user_name]").val(),
-			password: $("input[name=password]").val(),
-			institute: $("input[name=institute]").val()
-		};
+        //入力フォーマットが正しければ通信開始
+        $("#submit").on('click', function () {
+                //データ生成
+                var form_data = {
+                        name: $("input[name=name]").val(),
+                        password: $("input[name=password]").val(),
+                        institute: $("input[name=institute]").val()
+                };
 
-		//　通信開始
-		$.ajax({
-			url: 'php/register.php',
-			dataType: 'text',
-			type: 'POST',
-			data: form_data,
-			success: function (kgp_id) {
-				//$.cookie('kgp_id', kgp_id);
-				alert("success");
-			},
-			error: function () {
-				alert("failed");
-			},
-			timeout: 10000,
-		})
-	})
+                $.ajax({
+                        url: 'php/register.php',
+                        dataType: 'text',
+                        type: 'POST',
+                        data: form_data,
+                        success: function (msg) {
+                                console.log(msg);
+                        },
+                        error: function () {
+                                console.log("failed");
+                        },
+                        timeout: 10000,
+                })
+        })
 });
--- php/register.php
+++ php/register.php
@@ -1,47 +1,38 @@
 <?php
-	require_once 'macro.php';
+        require_once "macro.php";
 
-	//　データ受け取り
-	$user		=	(string)filter_input(INPUT_POST, 'user_name');
-	$password 	=	(string)filter_input(INPUT_POST, 'password');
-	$institute	=	(string)filter_input(INPUT_POST, 'institute');
+        $name=$_POST['name'];
+        $password=$_POST['password'];
+        $institute=$_POST['institute'];
 
-	// DB接続
-	try　{
-        $pdo = new PDO('mysql:dbname='.$db_name.';host='.$db_host,$db_hostname,$db_password,
-                array(  PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode='TRADITIONAL'",
-                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        $pdo->query('SET NAMES utf8');
-    }
-    catch(PDOException $e)　{　exit($e->getMessage());　}
+        try
+        {
+                $pdo = new PDO('mysql:dbname='.$db_name.';host='.$db_host,$db_hostname,$db_password,
+                                                array(  PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode='TRADITIONAL'",
+                                                                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                $pdo->query('SET NAMES utf8');
+        }
+        catch(PDOException $e)
+        {
+                exit($e->getMessage());
+        }
 
+        try
+        {
+                $stmt = $pdo->prepare("INSERT INTO ${TABLE_USERS}(name, password, institute)
+                                                                VALUES (:name, :password, :institute)");
+                $stmt->bindValue(':name', $name);
+                $stmt->bindValue(':password', $password);
+                $stmt->bindValue(':institute', $institute);
+                $stmt->execute();
 
-	// データ登録
-	try {
-		$stmt = $pdo->prepare("INSERT INTO ${TABLE_USERS}(name, password, institute) 
-								VALUES (:name, :password, :institute)");
-		$stmt->bindValue(':name', $user);
-		$stmt->bindValue(':password', $password);
-		$stmt->bindValue(':institute', $institute);
-		$stmt->execute();
+                //データベース接続終了
+                $pdo = null;
+        }
+        catch(PDOException $e)
+        {
+                exit($e->getMessage());
+        }
 
-		//データベース接続終了
-		$pdo = null;
-	}
-	catch(PDOException $e) { exit($e->getMessage()); }
-
-	// ID検索
-	// try　{
-	// 	$stmt = $pdo->prepare("SELECT id FROM ${TABLE_USERS}
-	// 							WHERE name = :name and password = :password");
-	// 	$stmt->bindValue(':name', $user_name);
-	// 	$stmt->bindValue(':password', $pass);
-	// 	$stmt->execute();
-
-	// 	$data = $stmt->fetch(PDO::FETCH_ASSOC)
-	// 	$kgp_id = $data['id'];
-	// }
-	// catch(PDOException $e){ exit($e->getMessage()); }
-
-	echo json_encode($name);
+        echo json_encode($name);
 ?>
