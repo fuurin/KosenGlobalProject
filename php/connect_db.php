3d69bb991b766ed50fe29cd74cc7e908c1a8f642
fuurin <komatsu0624@docomo.ne/jp>
2015-08-29 22:30:37 +0300

    cookieの利用

--- php/connect_db.php
+++ php/connect_db.php
@@ -4,8 +4,8 @@
 	
 	try {
         $pdo = new PDO('mysql:dbname='.$db_name.';host='.$db_host,$db_hostname,$db_password,
-                                        array(  PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode='TRADITIONAL'",
-                                                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                       array(  PDO::MYSQL_ATTR_INIT_COMMAND => "SET SESSION sql_mode='TRADITIONAL'",
+                       		   PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         $pdo->query('SET NAMES utf8');
     }
     catch(PDOException $e) { exit($e->getMessage()); }
