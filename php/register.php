1cae6c999fd7c669aa4abc01e49ae6d994b6343d
fuurin <komatsu0624@docomo.ne/jp>
2015-08-29 11:27:05 +0300

    戻したのでやってみる

--- php/register.php
+++ php/register.php
@@ -1,17 +1,15 @@
 <?php
 	require_once 'connect_db.php';
 
-<<<<<<< HEAD
+
 	//　データ受け取り
 	$user_name	=	(string)filter_input(INPUT_POST, 'user_name');
 	$pass 		=	(string)filter_input(INPUT_POST, 'password');
 	$institute	=	(string)filter_input(INPUT_POST, 'institute');
-=======
-	ChromePhp::log("connect");
 
 	$name=$_POST['name'];
 	$password=$_POST['password'];
-	$institute=$_POST['insitute'];
+	$institute=$_POST['institute'];
 
 	try
 	{
@@ -24,35 +22,36 @@
 	{
 		exit($e->getMessage());
 	}
->>>>>>> parent of ac4f64c... ユーザ登録機能
 
-	// データ登録
-	try
-	{
-		$stmt = $pdo->prepare("INSERT INTO ${TABLE_USERS}(name, password, institute) 
-								VALUES (:name, :password, :institute)");
-		$stmt->bindValue(':name', $user_name);
-		$stmt->bindValue(':password', $pass);
-		$stmt->bindValue(':institute', $institute);
-		$stmt->execute();
-	}
-	catch(PDOException $e)
-	{
-		exit($e->getMessage());
-	}
+// >>>>>>> parent of ac4f64c... ユーザ登録機能
 
-	// ID検索
-	// try　{
-	// 	$stmt = $pdo->prepare("SELECT id FROM ${TABLE_USERS}
-	// 							WHERE name = :name and password = :password");
-	// 	$stmt->bindValue(':name', $user_name);
-	// 	$stmt->bindValue(':password', $pass);
-	// 	$stmt->execute();
+// 	// データ登録
+// 	try
+// 	{
+// 		$stmt = $pdo->prepare("INSERT INTO ${TABLE_USERS}(name, password, institute) 
+// 								VALUES (:name, :password, :institute)");
+// 		$stmt->bindValue(':name', $user_name);
+// 		$stmt->bindValue(':password', $pass);
+// 		$stmt->bindValue(':institute', $institute);
+// 		$stmt->execute();
+// 	}
+// 	catch(PDOException $e)
+// 	{
+// 		exit($e->getMessage());
+// 	}
 
-	// 	$data = $stmt->fetch(PDO::FETCH_ASSOC)
-	// 	$kgp_id = $data['id'];
-	// }
-	// catch(PDOException $e){ exit($e->getMessage()); }
+// 	// ID検索
+// 	// try　{
+// 	// 	$stmt = $pdo->prepare("SELECT id FROM ${TABLE_USERS}
+// 	// 							WHERE name = :name and password = :password");
+// 	// 	$stmt->bindValue(':name', $user_name);
+// 	// 	$stmt->bindValue(':password', $pass);
+// 	// 	$stmt->execute();
+
+// 	// 	$data = $stmt->fetch(PDO::FETCH_ASSOC)
+// 	// 	$kgp_id = $data['id'];
+// 	// }
+// 	// catch(PDOException $e){ exit($e->getMessage()); }
 
 	//データベース接続終了
 	$pdo = null;
