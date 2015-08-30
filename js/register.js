ba5e2d156a7aea0276b90fbe6f34748b5832306d
fuurin <komatsu0624@docomo.ne/jp>
2015-08-30 04:00:59 +0300

    return falseをなくす

--- js/register.js
+++ js/register.js
@@ -26,8 +26,5 @@ $( function() {
                         },
                         timeout: 10000,
                 });
-
-                // Submit後、ページリロードされないらしい。
-	            return false;
-        })
+        });
 });
