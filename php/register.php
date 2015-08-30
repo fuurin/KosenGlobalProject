d7b7f5b8eb14655b6e23729364e8e851721f2c12
fuurin <komatsu0624@docomo.ne/jp>
2015-08-30 03:27:46 +0300

    ユーザに応じて名前が変わるTOPページ

--- php/register.php
+++ php/register.php
@@ -1,14 +1,12 @@
 <?php
         require_once "connect_db.php";
 
-        // データ受け取り
         $name=$_POST['name'];
         $password=$_POST['password'];
         $institute=$_POST['institute'];
 
-        // SQL実行
+        // 登録
         try {
-        	// 新規ユーザ登録
             $stmt = $pdo->prepare("INSERT INTO ${TABLE_USERS}(name, password, institute)
                                    VALUES (:name, :password, :institute)");
             $stmt->bindValue(':name', $name);
@@ -16,17 +14,22 @@
             $stmt->bindValue(':institute', $institute);
             $stmt->execute();
 
-            // IDを自動取得
    			$stmt = $pdo->prepare("SELECT id from ${TABLE_USERS} where name = :name ");
 			$stmt->bindValue(':name', $name);
 			$stmt->execute();
-			$data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+			if($data = $stmt->fetch(PDO::FETCH_ASSOC)){
+				$kgp_id = $data['id'];
+			}
+			else {
+				$kgp_id = "failed to get ID";
+			}
+
         }
         catch(PDOException $e) { exit($e->getMessage()); }
 
         //データベース接続終了
         $pdo = null;
 
-        // データはJSON文字列で返す
         echo json_encode($data);
 ?>
