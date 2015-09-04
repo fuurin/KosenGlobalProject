0def2da244b9ee7a9455684d7750893df8d99a3c
fuurin <komatsu0624@docomo.ne/jp>
2015-09-04 21:11:58 +0300

    registerのCSSをテーブルに

--- js/register.js
+++ js/register.js
@@ -1,23 +1,23 @@
 //phpに送信
 $( function() {
-        //入力フォーマットが正しければ通信開始
-        $("#submit").on('click', function () {
-                //データ生成
-                var form_data = {
-                        name: $("input[name=name]").val(),
-                        password: $("input[name=password]").val(),
-                        institute: $("input[name=institute]").val()
-                };
+	//入力フォーマットが正しければ通信開始
+	$("#submit").on('click', function () {
+		//データ生成
+		var form_data = {
+			name: $("input[name=name]").val(),
+			password: $("input[name=password]").val(),
+			institute: $("input[name=institute]").val()
+		};
 
-                $.ajax({
-                        url: 'php/register.php',
-                        type: 'POST',
-                        data: form_data,
-                        error: function (response) {
-                	        var res = JSON.parse(response);
-                                alert(res);
-                        },
-                        timeout: 10000,
-                });
-        });
+		$.ajax({
+			url: 'php/register.php',
+			type: 'POST',
+			data: form_data,
+			error: function (response) {
+				alert(res);
+				return false;
+			},
+			timeout: 10000,
+		});
+	});
 });
