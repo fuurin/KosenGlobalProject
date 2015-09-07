391d7ece1458c4d840c4ad2468f293872e06c1a1
fuurin <komatsu0624@docomo.ne/jp>
2015-09-07 13:27:33 +0300

    とりあえず動くかやってみよう

--- js/register.js
+++ js/register.js
@@ -6,28 +6,27 @@ $( function() {
 	};
 
 	// Ajax
-	var send_register_form = function (url) {
-		var form_data = {
-			name: $("input[name=name]").val(),
-			password: $("input[name=password]").val(),
-			institute: $("input[name=institute]").val()
-		};
-
+	var send_register_form = function (url, form_data) {
 		$.ajax({
 			url: url,
 			type: 'POST',
 			data: form_data,
 			error: function (response) {
 				alert(res);
-				return false;
 			},
 			timeout: 10000,
 		});
 	};
 
+	// submitボタン押下時の動作
 	$("#submit").on('click', function () {
 		if( check_register_form() ) {
-			send_register_form('php/register.php');
+			var form_data = {
+				name: $("input[name=name]").val(),
+				password: $("input[name=password]").val(),
+				institute: $("input[name=institute]").val()
+			};
+			send_register_form('php/register.php', form_data);
 		}
 	});
 });
