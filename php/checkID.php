d4335114c5480c3a2dc82b82af0c36f8b3120026
fuurin <komatsu0624@docomo.ne/jp>
2015-09-07 15:58:56 +0300

    resp→response

--- php/checkID.php
+++ php/checkID.php
@@ -1,5 +1,9 @@
 <?php
+	include "ChromePhp.php";
 	session_start();
 	$data["exist"] = isset($_SESSION["id"]);
+	ChromePhp::log("working");
+	ChromePhp::log($data["exist"]);
+	ChromePhp::log($_SESSION["id"]);
 	echo $data;
 ?>
